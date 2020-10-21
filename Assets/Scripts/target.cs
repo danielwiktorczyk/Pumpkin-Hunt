4570a6aa7e560694836d61884c999331cea3f9ca
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-10-21 15:37:34 -0400

    Add DIfferent Target Placeholders and Movement

--- Assets/Scripts/target.cs
+++ Assets/Scripts/target.cs
@@ -1,17 +1,32 @@
 ﻿using UnityEngine;
 
+
+//public class Witch : Target
+//{
+
+//}
 public class Target : MonoBehaviour
 {
     private GameObject gameController;
     private Score score;
+    [SerializeField]
     private int pointWorth = 3;
+    [SerializeField]
     private int shotsRequired = 1;
+    //[SerializeField]
+    //private TargetMovement targetMovement; // WitchMovement : TargetMovement
 
-    // Start is called before the first frame update
     void Start()
     {
         gameController = GameObject.FindGameObjectWithTag("GameController");
         score = gameController.GetComponent<Score>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(Time.deltaTime * 0.1f * Vector2.up);
+        //* from current position A, move to position B in Xseconds, in a sinuloadal way 
+
     }
 
     public void HitTarget()
